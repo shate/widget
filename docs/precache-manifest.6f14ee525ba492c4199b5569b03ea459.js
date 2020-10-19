93bd2cd21578e2ce7ecc615886386667a09981d7
shate <shate@ya.ru>
2020-10-19 16:47:13 +0300

    init

---
+++ docs/precache-manifest.6f14ee525ba492c4199b5569b03ea459.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b43b142d02155921e4d0c3723d22018",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9bcc0da7db19acdb37",
+    "url": "/static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "54254b428340755eaa58",
+    "url": "/static/js/2.81725db8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.81725db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f9bcc0da7db19acdb37",
+    "url": "/static/js/main.9d3d96ca.chunk.js"
+  },
+  {
+    "revision": "09ac21942ba91407d728",
+    "url": "/static/js/runtime-main.2a542afb.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,126 @@
-import React from 'react';
-import './App.scss';
+import React, { useEffect, useState } from 'react';
+import NumberFormat from 'react-number-format';
+import styles from './App.module.scss';
 
 function App() {
+    const [statistic, setStatistic] = useState(false);
+    useEffect(()=> {
+        fetch(
+            `https://testback.coinmarketrate.com/api/asset_statistic/bitcoin`
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setStatistic(data.data);
+            })
+            .catch(e => console.error(e));
+    }, [])
   return (
-    <div className="widget">
+      statistic
+          ? <div
+            className={`${styles.d_flex} ${styles.flex_column} ${styles.justify_content_between} ${styles.h_100} ${styles.bg_white} ${styles.p_4} ${styles.border} ${styles.border_border1}`}>
+            <div className={`${styles.d_flex} ${styles.mb_5} mb-sm-0`}>
+                <div className={styles.tab_tools_ico}>
+                    <img src={((statistic.name ? '/icons/' + statistic.name + '.png' : null))}/>
+                </div>
+                <div className={styles.ml_3}>
+                    <small className={styles.text_grey3}>
+                        {(statistic.symbol ? statistic.symbol : null)}
+                    </small>
+                    <h3 className={`${styles.text_uppercase} ${styles.text_grey4} ${styles.mb_0}`}>
+                        {(statistic.name ? statistic.name : null)}
+                    </h3>
+                </div>
+            </div>
+            <div className={`${styles.d_flex} ${styles.justify_content_between}`}>
+                <div>
+                    <div className={`${styles.text_14} ${styles.text_grey3}`}>
+                        Rank
+                    </div>
+                    <strong className={`${styles.text_18} ${styles.text_grey4}`}>15.61M</strong>
+                    <small><strong className={`${styles.text_grey4} ${styles.text_uppercase}`}>usd</strong></small>
+                </div>
+                <div>
+                    <div className={`${styles.text_14} ${styles.text_grey3}`}>
+                        Cap
+                    </div>
+                    <strong className={`${styles.text_18} t${styles.text_grey4}`}>${statistic.stat && parseFloat(statistic.stat.market_cap/1000000).toFixed(2)}M</strong>
+                </div>
+                <div>
+                    <div className={`${styles.text_14} ${styles.text_grey3}`}>
+                        Volume
+                    </div>
+                    <strong className={`${styles.text_18} ${styles.text_grey4}`}>${statistic.stat && parseFloat(statistic.stat.volume24h/1000).toFixed(2)}K</strong>
+                </div>
+            </div>
+            <div className={`${styles.d_flex} ${styles.justify_content_end} ${styles.p_0} ${styles.currency_head_info}`}>
+                <div className={styles.currency_head_stats}>
+                    <div className={styles.text_right}>
+                  <span
+                      className={
+                          'currency-head-stats-usd-change ' +
+                          (statistic.stat ? (statistic.stat.percent24h >= 0
+                              ? styles.text_success
+                              : styles.text_danger) : styles.text_success)
+                      }
+                  >
+                    <NumberFormat
+                        value={(statistic.stat ? statistic.stat.percent24h : 0)}
+                        displayType={'text'}
+                        thousandSeparator={false}
+                        suffix={'%'}
+                        decimalScale={2}
+                        fixedDecimalScale={true}
+                    />
+                  </span>
+                        <span className="currency-head-stats-usd-price">
+                    <NumberFormat
+                        value={(statistic.stat ? statistic.stat.price : 0)}
+                        displayType={'text'}
+                        thousandSeparator={false}
+                        prefix={'$'}
+                        decimalScale={statistic.stat ? (statistic.stat.price >= 1 ? 2 : 6) : 2}
+                        fixedDecimalScale={true}
+                    />
+                  </span>
+                        <span className="currency-head-stats-usd-id">USD</span>
+                    </div>
+                    <div className="currency-head-stats-btc">
+                  <span
+                      className={`${styles.currency_head_stats_usd_change} ${statistic.stat ? (statistic.stat.percent24h_btc >= 0
+                          ? styles.text_success
+                          : styles.text_danger) : styles.text_success}`
 
-      Widget
-    </div>
+                      }
+                  >
+                    <NumberFormat
+                        value={(statistic.stat ? statistic.stat.percent24h_btc : 0)}
+                        displayType={'text'}
+                        thousandSeparator={false}
+                        suffix={'%'}
+                        decimalScale={2}
+                        fixedDecimalScale={true}
+                    />
+                  </span>
+                        <span className="currency-head-stats-btc-price">
+                    <NumberFormat
+                        value={(statistic.stat ? statistic.stat.price_btc : 0)}
+                        displayType={'text'}
+                        thousandSeparator={false}
+                        prefix={''}
+                        decimalScale={statistic.stat ? (statistic.stat.price_btc >= 1 ? 2 : 6) : 2}
+                        fixedDecimalScale={true}
+                    />
+                  </span>
+                        <span className="currency-head-stats-btc-id">BTC</span>
+                    </div>
+                </div>
+            </div>
+            <a href="">
+                <small className={styles.text_primary}>Powered by CoinMarketRate</small>
+            </a>
+        </div>
+          : <div>Loading...</div>
+
   );
 }
 
